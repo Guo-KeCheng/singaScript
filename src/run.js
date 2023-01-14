43a605796f9d229f3d71f21cde154ba4b0cdd92d
sikai00 <sikai4111@gmail.com>
2023-01-14 18:31:49 +0800

    Add basic functioning end point

--- src/run.js
+++ src/run.js
@@ -1,25 +1,55 @@
 const express = require("express");
+const fs = require("fs/promises");
+const { exec } = require("child_process");
+const Joi = require("joi");
+
 const router = express.Router();
-const { exec } = require('child_process');
+const reqSchema = Joi.object({ userInput: Joi.string().required() });
+
+router.use(express.json());
 
 router.get("/", function (req, res) {
-  res.send("POST data instead");
+  res.status(404).send("GET requests are not supported.");
 });
 
 router.post("/", function (req, res) {
-  exec("ls -al", (err, stdout, stderr) => {
-    if (err) {
-      console.error(`error: ${error.message}`);
-      return;
-    }
-  
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      return;
-    }
+  const { error, value } = reqSchema.validate(req.body);
 
-    res.send(`stdout:\n${stdout}`);
-  });
+  if (error) {
+    res.status(404).send(error.details[0].message);
+  }
+
+  runCode(res, value.userInput);
 });
+
+function runCode(res, value) {
+  const lahFile = createLahFile(value);
+
+  lahFile
+    .then(() =>
+      exec(
+        "python3 ../singaScript_interpreter/shell.py temp.lah",
+        (err, stdout, stderr) => {
+          if (err) {
+            console.error(`error: ${err.message}`);
+            return;
+          }
+
+          if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+          }
+
+          const returnResult = { results: stdout };
+          res.json(returnResult);
+        }
+      )
+    )
+    .catch((error) => console.log(error));
+}
+
+function createLahFile(value) {
+  return fs.writeFile("temp.lah", value);
+}
 
 module.exports = router;
