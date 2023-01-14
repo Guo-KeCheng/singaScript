995ab75880ca285265955a7d303463c2153063fd
Guo-KeCheng <guo.kecheng99@gmail.com>
2023-01-14 14:06:01 +0800

    Update shell to read file name

--- singaScript_interpreter/shell.py
+++ singaScript_interpreter/shell.py
@@ -1,14 +1,17 @@
 import basic
+import sys
 
-while True:
-	text = input('basic > ')
-	if text.strip() == "": continue
-	result, error = basic.run('<stdin>', text)
+text = sys.argv[1]
+file = open(text)
+content = file.read()
+print(content)
 
-	if error:
-		print(error.as_string())
-	elif result:
-		if len(result.elements) == 1:
-			print(repr(result.elements[0]))
-		else:
-			print(repr(result))
+result, error = basic.run('<stdin>', content)
+
+if error:
+    print(error.as_string())
+elif result:
+    if len(result.elements) == 1:
+        print(repr(result.elements[0]))
+    else:
+        print(repr(result))
