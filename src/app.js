9a0faf480f932ed187e083b88a8a9b9178c6b473
sikai00 <sikai4111@gmail.com>
2023-01-15 08:16:12 +0800

    Update CORS settings

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(logger("dev"));
 app.use(
   cors({
-    origin: "http://localhost:3002",
+    origin: "https://singa-script.vercel.app/",
   })
 );
 
